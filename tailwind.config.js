affe1fcac71c212d2dfcee48e021a32e9835c64d
quân nguyễn <nhquan0911@gmail.com>
2023-08-14 10:41:55 +0700

    oke

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "jit",
+    purge: ["./**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
